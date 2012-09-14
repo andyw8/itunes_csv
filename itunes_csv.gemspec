aed0287d6778ad0a13de3b302d4a5669d2350dc1
Andy Waite <github.aw@andywaite.com>
2012-09-14 11:20:02 +0100

    First release

--- itunes_csv.gemspec
+++ itunes_csv.gemspec
@@ -4,9 +4,9 @@ require File.expand_path('../lib/itunes_csv/version', __FILE__)
 Gem::Specification.new do |gem|
   gem.authors       = ["Andy Waite"]
   gem.email         = ["github.aw@andywaite.com"]
-  gem.description   = %q{TODO: Write a gem description}
-  gem.summary       = %q{TODO: Write a gem summary}
-  gem.homepage      = ""
+  gem.description   = "Export iTunes Library XML data to CSV"
+  gem.summary       = "Export iTunes Library XML data to CSV"
+  gem.homepage      = "https://github.com/andyw8/itunes_csv"
 
   gem.files         = `git ls-files`.split($\)
   gem.executables   = gem.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
@@ -14,5 +14,5 @@ Gem::Specification.new do |gem|
   gem.name          = "itunes_csv"
   gem.require_paths = ["lib"]
   gem.version       = ItunesCsv::VERSION
-  gem.add_dependency "itunes-library"
+  gem.add_runtime_dependency "itunes-library"
 end
--- lib/itunes_csv.rb
+++ lib/itunes_csv.rb
@@ -1,16 +1,40 @@
 require "itunes_csv/version"
 require "itunes/library"
 require "csv"
+require 'optparse'
 
 module ItunesCsv
-  DefaultPath = "~/Music/iTunes/iTunes Music Library.xml"
   class CommandLine
     def self.invoke(args)
-      library_path = args.first || File.expand_path(DefaultPath)
+      options = {}
+
+      opts = OptionParser.new do |opts|
+        options[:fields] = ['name', 'artist']
+        opts.on( '-f', '--fields name,track,year', Array, "List of fields (default: #{options[:fields].join(',')})" ) do |list|
+          options[:fields] = list
+        end
+
+        options[:path] = '~/Music/iTunes/iTunes Music Library.xml'
+        opts.on( '-p', '--path PATH', "Path to iTunes XML file (default: #{options[:path]})" ) do |path|
+          options[:path] = path
+        end
+      end
+
+      opts.parse!
+
+      library_path = File.expand_path(options[:path])
       library = ITunes::Library.load(library_path)
       csv_string = CSV.generate do |csv|
+        csv << options[:fields]
       	library.music.tracks.each do |t|
-  		    csv << [t.name]
+          row = options[:fields].map do |f|
+            unless t.respond_to?(f.to_sym)
+              puts "Unknown field: #{f}"
+              exit
+            end
+            t.send(f.to_sym)
+          end
+  		    csv << row
   		  end
   	  end
   	  puts csv_string
